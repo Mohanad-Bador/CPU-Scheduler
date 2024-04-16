0d2397780c0345792e527eaf7ef2e88b2c9c62d7
Mohanad Ahmed <mohanadbador.ma@gmail.com>
2024-04-16 10:29:59 +0200

    Priority Queue Class and functions

---
+++ priorityQ.h
@@ -0,0 +1,20 @@
+#ifndef PRIORITYQ_H_INCLUDED
+#define PRIORITYQ_H_INCLUDED
+
+#include <vector>
+#include"process.h"
+
+using namespace std;
+
+bool comparePriority(const process& p1, const process& p2);
+
+class PriorityQueue {
+private:
+    vector<process> processes;
+public:
+    void insert(const process& p);
+    process extractMin();
+    bool isEmpty();
+};
+
+#endif // PRIORITYQ_H_INCLUDED
