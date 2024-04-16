de90b0e638369889e59744d35860789cdda35c78
Mohanad Ahmed <mohanadbador.ma@gmail.com>
2024-04-16 10:28:30 +0200

    Procces Class

---
+++ process.cpp
@@ -0,0 +1,85 @@
+#ifndef PROCESS_CPP_INCLUDED
+#define PROCESS_CPP_INCLUDED
+
+#include "process.h"
+
+
+// Default constructor
+process::process() {
+    pid = 0;
+    arrival_time = -1;
+    burst_time = -1;
+    priority = 0;
+}
+
+// Parameterized constructor
+process::process(int pid, int arrival_time, int burst_time) {
+    this->pid = pid;
+    this->arrival_time = arrival_time;
+    this->burst_time = burst_time;
+}
+
+
+// Parameterized constructor with Priority
+process::process(int pid, int arrival_time, int burst_time, int priority) {
+    this->pid = pid;
+    this->arrival_time = arrival_time;
+    this->burst_time = burst_time;
+    this->priority = priority;
+}
+
+// Getter methods
+int process::getPid() const {
+    return pid;
+}
+
+int process::getArrivalTime() const {
+    return arrival_time;
+}
+
+int process::getBurstTime() const {
+    return burst_time;
+}
+
+int process::getStartTime() const {
+    return start_time;
+}
+
+int process::getFinishTime() const {
+    return finish_time;
+}
+
+int process::getPriority() const {
+    return priority;
+}
+
+void process::setBurstTime(int new_burst_time){
+    this->burst_time = new_burst_time;
+}
+
+void process::setStartTime(int new_start_time){
+    this->start_time = new_start_time;
+}
+
+void process::setFinishTime(int new_finish_time){
+    this->finish_time = new_finish_time;
+}
+
+void process::setPriority(int new_priority){
+    this->priority = new_priority;
+}
+
+
+int process::Waiting_Time(){
+    return (finish_time - arrival_time - burst_time);
+}
+
+int process::TurnAround_Time(){
+    return (finish_time - arrival_time);
+}
+
+void process::printProcess(){
+    cout << "Process ID: " << pid << ", Arrival Time: " << arrival_time << ", Burst Time: " << burst_time << ", Priority: " << priority << endl;
+}
+
+#endif // PROCESS_CPP_INCLUDED
---
+++ process.h
@@ -0,0 +1,41 @@
+#ifndef PROCESS_H_INCLUDED
+#define PROCESS_H_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+class process{
+private:
+    int pid;
+    int arrival_time;
+    int burst_time;
+    int start_time;
+    int finish_time;
+    int priority;
+
+
+public:
+    process();
+    process(int pid, int arr_time, int burst_time);
+    process(int pid, int arr_time, int burst_time, int priority);
+
+    int getPid() const;
+    int getArrivalTime() const;
+    int getBurstTime() const;
+    int getStartTime() const;
+    int getFinishTime() const;
+    int getPriority() const;
+
+    void setBurstTime(int new_burst_time);
+    void setStartTime(int new_start_time);
+    void setFinishTime(int new_finish_time);
+    void setPriority(int new_priority);
+
+    int Waiting_Time();
+    int TurnAround_Time();
+
+    void printProcess ();
+
+};
+
+#endif // PROCESS_H_INCLUDED
