b101bfb9a7cacfb6b432ca1cfcfffce76a6962dd
Mohanad Ahmed <mohanadbador.ma@gmail.com>
2024-04-16 12:31:06 +0200

    Added the average waiting time and the average turnaround time

--- non-preemptive-priority-scheduler.h
+++ non-preemptive-priority-scheduler.h
@@ -7,33 +7,57 @@
 
 void NP_Priority(vector<process> &inQ , int &current_time){
     PriorityQueue Q;
+    vector<int> WaitingTime;
+    vector<int> TurnAroundTime;
+    cout<<"Gantt Chart For NP Priority Scheduler"<<endl;
+    cout<<current_time<<' ';
     while(!inQ.empty()){
         for (auto it = inQ.begin(); it != inQ.end();) {
             const auto& process = *it;
-            //cout << "inside for loop" << endl;
             if (process.getArrivalTime() <= current_time) {
                 Q.insert(process);
                 it = inQ.erase(it); // Erase and update iterator
-                //cout << "inside if" << endl;
             } else {
                 ++it; // Move to the next element
             }
         }
-        if(Q.isEmpty()&&!inQ.empty())
+        if(Q.isEmpty()&&!inQ.empty()){
             current_time++;
+            cout<<current_time<<' ';
+        }
         if(!Q.isEmpty()){
-            cout << "Current Time: " << current_time << endl;
             process P = Q.extractMin();
-            cout << "Executing Process ID: " << P.getPid() << endl;
+            P.setStartTime(current_time);
             current_time = current_time + P.getBurstTime();
+            P.setFinishTime(current_time);
+            WaitingTime.push_back(P.Waiting_Time());
+            TurnAroundTime.push_back(P.TurnAround_Time());
+            cout<<'P'<<P.getPid()<<' '<<current_time<<' ';
         }
     }
     while(inQ.empty()&&!Q.isEmpty()){
-        cout << "Current Time: " << current_time << endl;
         process P = Q.extractMin();
-        cout << "Executing Process ID: " << P.getPid() << endl;
+        P.setStartTime(current_time);
         current_time = current_time + P.getBurstTime();
+        P.setFinishTime(current_time);
+        WaitingTime.push_back(P.Waiting_Time());
+        TurnAroundTime.push_back(P.TurnAround_Time());
+        cout<<'P'<<P.getPid()<<' '<<current_time<<' ';
     }
+    cout<<endl;
+    int TotalWaitingTime=0;
+    int TotalTurnAroundTime=0;
+    for(int W : WaitingTime)
+        TotalWaitingTime += W;
+    for(int T : TurnAroundTime)
+        TotalTurnAroundTime += T;
+//    cout<<"Total Waiting Time: "<<TotalWaitingTime<<endl;
+//    cout<<"Total Turn Around Time: "<<TotalTurnAroundTime<<endl;
+    int AverageTotalWaitingTime = TotalWaitingTime/WaitingTime.size();
+    int AverageTotalTurnAroundTime = TotalTurnAroundTime/TurnAroundTime.size();
+    cout<<"Average Total Waiting Time: "<<AverageTotalWaitingTime<<endl;
+    cout<<"Average Total Turn Around Time: "<<AverageTotalTurnAroundTime<<endl;
+
 
 }
 
--- preemptive-priority-scheduler.h
+++ preemptive-priority-scheduler.h
@@ -7,41 +7,72 @@
 
 void P_Priority(vector<process> &inQ , int &current_time){
     PriorityQueue Q;
+    vector<int> WaitingTime;
+    vector<int> TurnAroundTime;
+    cout<<"Gantt Chart For P Priority Scheduler"<<endl;
+    cout<<current_time<<' ';
     while(!inQ.empty()){
         for (auto it = inQ.begin(); it != inQ.end();) {
             const auto& process = *it;
-            //cout << "inside for loop" << endl;
             if (process.getArrivalTime() <= current_time) {
                 Q.insert(process);
                 it = inQ.erase(it); // Erase and update iterator
-                //cout << "inside if" << endl;
             } else {
                 ++it; // Move to the next element
             }
         }
-        if(Q.isEmpty()&&!inQ.empty())
+        if(Q.isEmpty()&&!inQ.empty()){
             current_time++;
+            cout<<current_time<<' ';
+        }
         if(!Q.isEmpty()){
             process P = Q.extractMin();
-            if(P.getBurstTime()>0){
-            cout << "Current Time: " << current_time << endl;
-            cout << "Executing Process ID: " << P.getPid() << endl;
-                P.setBurstTime(P.getBurstTime()-1);
+            if(P.getRemainingTime()==P.getBurstTime()){
+                P.setStartTime(current_time);
+            }
+            if(P.getRemainingTime()>0){
+                P.setRemainingTime(P.getRemainingTime()-1);
                 Q.insert(P);
-            current_time++;
+                current_time++;
+                cout<<'P'<<P.getPid()<<' '<<current_time<<' ';
+            }
+            else{
+                P.setFinishTime(current_time);
+                WaitingTime.push_back(P.Waiting_Time());
+                TurnAroundTime.push_back(P.TurnAround_Time());
             }
         }
     }
     while(inQ.empty()&&!Q.isEmpty()){
-            process P = Q.extractMin();
-            if(P.getBurstTime()>0){
-            cout << "Current Time: " << current_time << endl;
-            cout << "Executing Process ID: " << P.getPid() << endl;
-                P.setBurstTime(P.getBurstTime()-1);
-                current_time++;
-                Q.insert(P);
-            }
+        process P = Q.extractMin();
+        if(P.getRemainingTime()==P.getBurstTime()){
+            P.setStartTime(current_time);
+        }
+        if(P.getRemainingTime()>0){
+            P.setRemainingTime(P.getRemainingTime()-1);
+            Q.insert(P);
+            current_time++;
+            cout<<'P'<<P.getPid()<<' '<<current_time<<' ';
+        }
+        else{
+            P.setFinishTime(current_time);
+            WaitingTime.push_back(P.Waiting_Time());
+            TurnAroundTime.push_back(P.TurnAround_Time());
+        }
     }
+    cout<<endl;
+    int TotalWaitingTime=0;
+    int TotalTurnAroundTime=0;
+    for(int W : WaitingTime)
+        TotalWaitingTime += W;
+    for(int T : TurnAroundTime)
+        TotalTurnAroundTime += T;
+//    cout<<"Total Waiting Time: "<<TotalWaitingTime<<endl;
+//    cout<<"Total Turn Around Time: "<<TotalTurnAroundTime<<endl;
+    int AverageTotalWaitingTime = TotalWaitingTime/WaitingTime.size();
+    int AverageTotalTurnAroundTime = TotalTurnAroundTime/TurnAroundTime.size();
+    cout<<"Average Total Waiting Time: "<<AverageTotalWaitingTime<<endl;
+    cout<<"Average Total Turn Around Time: "<<AverageTotalTurnAroundTime<<endl;
 
 }
 
