361e57f0b76d51d9b9375e673417c7acda4b43ad
Mohammed Magdy <130768316+MohammedMagdyy@users.noreply.github.com>
2024-04-17 07:29:39 +0200

    Add files via upload

--- Round_Roubin.cpp
+++ Round_Roubin.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <vector>
-#include <string>
-
+#include <queue>
 using namespace std;
 
 class Process {
 public:
-    string pid;
+    int pid;
     int arrival_time;
     int burst_time;
     int start_time;
     int finish_time;
     int priority;
 
-    Process(string p, int a, int b) : pid(p), arrival_time(a), burst_time(b), priority(0) {}
+    Process(char pid, int AT, int BT) : pid(pid), arrival_time(AT), burst_time(BT), priority(0) {}
 
     int Waiting_Time() {
         return finish_time - arrival_time - burst_time;
@@ -24,19 +23,20 @@ public:
     }
 };
 
-vector<int> chart = {0};
 
-vector<Process> Circular_Shift_Left(const vector<Process>& list) {
-    vector<Process> shiftedList = list;
-    Process temp = shiftedList[0];
-    for (size_t i = 0; i < shiftedList.size() - 1; i++) {
-        shiftedList[i] = shiftedList[i + 1];
+vector<int> chart = {0}; // Gantt chart should be a vector of integers
+
+void Circular_Shift_Left(vector<Process>&list) {
+    int temp = list[0].pid;
+    for (size_t i = 0; i < list.size() - 1; i++) {
+        list[i].pid = list[i + 1].pid;
     }
-    shiftedList[shiftedList.size() - 1] = temp;
-    return shiftedList;
+    list[list.size() - 1].pid = temp;
 }
 
-vector<Process> RR(int timequantum, vector<Process>& Waiting_queue) {
+
+
+vector<int> RR(int timequantum, vector<Process> &Waiting_queue) {
     vector<Process> Ready_queue;
     int n = Waiting_queue.size();
     int time = 0;
@@ -49,11 +49,11 @@ vector<Process> RR(int timequantum, vector<Process>& Waiting_queue) {
     while (finished_processes < n) {
         for (int i = arrived_processes; i < n; i++) {
             if (time >= Waiting_queue[i].arrival_time) {
-                Ready_queue.push_back(Waiting_queue[i]);
+                Ready_queue[i]=Waiting_queue[i];  //@@@@@@@@@@@@@@@@
                 arrived_processes++;
                 ready_processes++;
             }
-      }
+        }
 
         if (ready_processes < 1) {
             time++;
@@ -61,19 +61,19 @@ vector<Process> RR(int timequantum, vector<Process>& Waiting_queue) {
         }
 
         if (start == true) {
-            Ready_queue = Circular_Shift_Left(Ready_queue);
+            Circular_Shift_Left(Ready_queue);
         }
 
         if (Ready_queue[0].burst_time > 0) {
             if (Ready_queue[0].burst_time > q) {
-                for (int g = time; g < time + q; g++) {       //*******
-                    chart.push_back(stoi(Ready_queue[0].pid));
+                for (int g = time; g < time + q; g++) {
+                    chart.push_back(Ready_queue[0].pid);
                     time += q;
                     Ready_queue[0].burst_time -= q;
                 }
-
             } else {
                 for (int g = time; g < time + Ready_queue[0].burst_time; g++) {
+                    chart.push_back(Ready_queue[0].pid);
                     time += Ready_queue[0].burst_time;
                     Ready_queue[0].burst_time = 0;
                     Ready_queue[0].finish_time = time;
@@ -84,27 +84,47 @@ vector<Process> RR(int timequantum, vector<Process>& Waiting_queue) {
             }
         }
     }
-
-    return Waiting_queue; // Return Waiting_queue or Ready_queue?
+    return chart;
 }
 
+
 int main() {
-    vector<Process> processes = {
-            {"1", 0, 5},
-            {"2", 1, 4},
-            {"3", 2, 2},
-            {"4", 3, 1}
-    };
+    int num_processes;
+    cout << "Enter the number of processes: ";
+    cin >> num_processes;
+
+    vector<Process> processes;  // Initialize an empty vector of processes
+
+    for (int i = 0; i < num_processes; i++) {
+        int pid, arrival_time, burst_time;
+        cout << "Enter Process ID for Process " << i + 1 << ": ";
+        cin >> pid;
+
+        cout << "Enter Arrival Time for Process " << i + 1 << ": ";
+        cin >> arrival_time;
+
+        cout << "Enter Burst Time for Process " << i + 1 << ": ";
+        cin >> burst_time;
+
+        processes.emplace_back(pid, arrival_time, burst_time);  // Add a new process to the vector
+
+        // Debug output to verify process details
+        cout << "Process " << i + 1 << " details: PID=" << processes[i].pid
+             << ", Arrival Time=" << processes[i].arrival_time
+             << ", Burst Time=" << processes[i].burst_time << endl;
+    }
 
     int timeQuantum = 2;
 
-    RR(timeQuantum, processes);
+    vector<int> Result = RR(timeQuantum, processes);
 
     cout << "Gantt Chart: ";
-    for (int process : chart) {
-        cout << process << " ";
+    for (int i = 0; i < Result.size(); i++) {
+        cout << Result[i] << " | ";
     }
-    cout << endl;
 
     return 0;
 }
+
+
+
