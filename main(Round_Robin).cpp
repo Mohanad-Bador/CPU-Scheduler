1572b6db59f813558c33e399b02574a9b02d753a
Mohammed Magdy <130768316+MohammedMagdyy@users.noreply.github.com>
2024-04-18 03:09:40 +0200

    Add files via upload

--- main(Round_Robin).cpp
+++ main(Round_Robin).cpp
@@ -11,10 +11,10 @@ int main(){
     vector<process>processes;
     // Initialize an empty vector of processes
     process p1={1,0,5};
-    process p2={2,1,3};
-    process p3={3,3,6};
-    process p4={4,5,1};
-    process p5={5,6,4};
+    process p2={2,0,3};
+    process p3={3,0,6};
+    process p4={4,0,1};
+    process p5={5,0,4};
     processes.push_back(p1);
     processes.push_back(p2);
     processes.push_back(p3);
@@ -26,7 +26,7 @@ int main(){
     cout << "Gantt Chart: ";
 
     for (int i = 0; i < Result.size(); i++) {
-        cout << Result[i] << " | ";
+        cout<< " | " << Result[i] << " | ";
     }
 
     return 0;
