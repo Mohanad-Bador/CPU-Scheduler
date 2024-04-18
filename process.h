0e52813ef927f10cc87128b7babe301283b4dd2d
Mohammed Magdy <130768316+MohammedMagdyy@users.noreply.github.com>
2024-04-18 02:02:20 +0200

    Add files via upload

--- process.h
+++ process.h
@@ -18,41 +18,8 @@ struct process
     // QString color;
 };
 
+void Circular_Shift_Left(vector<process>&list);
+vector<int> RR(int timequantum, vector<process> &Waiting_queue);
+
 #endif // PROCESS_H_INCLUDED
 
-// class process{
-// private:
-//     int pid;
-//     int arrival_time;
-//     int burst_time;
-//     int remaining_time;
-//     int start_time;
-//     int finish_time;
-//     int priority;
-//
-//
-// public:
-//     process();
-//     process(int pid, int arr_time, int burst_time);
-//     process(int pid, int arr_time, int burst_time, int priority);
-//
-//     int getPid() const;
-//     int getArrivalTime() const;
-//     int getBurstTime() const;
-//     int getRemainingTime() const;
-//     int getStartTime() const;
-//     int getFinishTime() const;
-//     int getPriority() const;
-//
-//     void setBurstTime(int new_burst_time);
-//     void setRemainingTime(int new_remaining_time);
-//     void setStartTime(int new_start_time);
-//     void setFinishTime(int new_finish_time);
-//     void setPriority(int new_priority);
-//
-//     int Waiting_Time();
-//     int TurnAround_Time();
-//
-//     void printProcess ();
-//
-// };
